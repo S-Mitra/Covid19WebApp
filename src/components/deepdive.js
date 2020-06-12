d8e0ba783fdae73535640a47df6e45f4361fb91c
S-Mitra <sneha.mitra2607@gmail.com>
2020-06-12 16:55:28 +0530

    carausal added

--- src/components/deepdive.js
+++ src/components/deepdive.js
@@ -22,6 +22,42 @@ function DeepDive() {
     }
   }, [fetched]);
 
+  let slideIndex = 1;
+  showSlides(slideIndex);
+
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  function showSlides(n) {
+    let i;
+    // const slides = document.getElementById('mySlides')
+    let slides = document.getElementsByClassName('mySlides');
+    let dots = document.getElementsByClassName('dot');
+    if (slides.length != 0 && dots.length != 0) {
+      if (n > slides.length) {
+        slideIndex = 1;
+      }
+      if (n < 1) {
+        slideIndex = slides.length;
+      }
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(' active', '');
+      }
+      slides[slideIndex - 1].style.display = 'block';
+      dots[slideIndex - 1].className += ' active';
+    }
+  }
+
   const getStates = async () => {
     try {
       const [
@@ -49,44 +85,59 @@ function DeepDive() {
         <meta name="title" content="Deep Dive - covid19india.org" />
       </Helmet>
 
-      <section className="cards">
-        <div className="card fadeInUp" style={{animationDelay: '0.7s'}}>
+      <div className="slideshow-container">
+        <div className="mySlides fade">
           <TotalConfirmedChart title="Total Cases" timeseries={timeseries} />
         </div>
 
-        <div className="card fadeInUp" style={{animationDelay: '0.7s'}}>
+        <div className="mySlides fade">
           <DailyConfirmedChart title="Daily Cases" timeseries={timeseries} />
         </div>
 
-        <div
-          className="card card-big fadeInUp"
-          style={{animationDelay: '0.7s'}}
-        >
+        <div className="mySlides fade card-big">
           <AllStatesChart
             title="Total Cases by State"
             data={statesTimeSeries}
           />
         </div>
 
-        <div className="card fadeInUp" style={{animationDelay: '0.7s'}}>
+        <div className="mySlides fade">
           <GrowthTrendChart
             title="States - Growth Trend"
             data={statesTimeSeries}
           />
         </div>
 
-        <div className="card fadeInUp" style={{animationDelay: '0.7s'}}>
+        <div className="mySlides fade">
           <GenderChart title="Patient Gender" data={rawData} />
         </div>
 
-        <div className="card fadeInUp" style={{animationDelay: '0.7s'}}>
+        <div className="mySlides fade">
           <AgeChart title="Patients by Age" data={rawData} />
         </div>
 
-        <div className="card fadeInUp" style={{animationDelay: '0.7s'}}>
+        <div className="mySlides fade">
           <NationalityChart title="Patients by  Nationality" data={rawData} />
         </div>
-      </section>
+        {/* Next and previous buttons */}
+        <a className="prev" onClick={() => plusSlides(-1)}>
+          &#10094;
+        </a>
+        <a className="next" onClick={() => plusSlides(1)}>
+          &#10095;
+        </a>
+      </div>
+      <br />
+      {/* The dots/circles  */}
+      <div style={{textAlign: 'center'}}>
+        <span className="dot" onClick={() => currentSlide(1)}></span>
+        <span className="dot" onClick={() => currentSlide(2)}></span>
+        <span className="dot" onClick={() => currentSlide(3)}></span>
+        <span className="dot" onClick={() => currentSlide(4)}></span>
+        <span className="dot" onClick={() => currentSlide(5)}></span>
+        <span className="dot" onClick={() => currentSlide(6)}></span>
+        <span className="dot" onClick={() => currentSlide(7)}></span>
+      </div>
     </div>
   );
 }
